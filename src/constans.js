e222390d6e371c4579e5f778a8934bfe24679488
ArthurDorokhin <a.u.dorokhin@gmail.com>
2020-08-09 12:30:43 +0300

    app

--- src/constans.js
+++ src/constans.js
@@ -1,5 +1,5 @@
 const usersData = [
-    { id: 1, name: 'Dima', username: 'dima@1999' },
+    { id: 1, name: 'Dima', username: 'dima@19991' },
     { id: 2, name: 'Taras', username: 'Rokoko' },
     { id: 3, name: 'Benito', username: 'benifest123' }
 ]
